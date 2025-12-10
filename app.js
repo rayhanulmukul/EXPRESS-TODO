14a09334fb33028e3fcc168a27be6b5e2b91e4fa
Md Rayhanul Islam <73143090+rayhanulmukul@users.noreply.github.com>
2025-12-10 15:06:52 +0600

    json file get post

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
 import express from 'express';
+import fs from 'fs';
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+const findNextId = (tasks) => {
+    if (tasks.length === 0) return 1;
+    return Math.max(...tasks.map(task => task.id)) + 1;
+}
+
+if (!fs.existsSync('tasks.json')) {
+    fs.writeFileSync('tasks.json', '[]');
+}
+
 app.get('/', (req, res) => {
-    const tasks = [
-        { id: 1, title: 'Task One', completed: false },
-        { id: 2, title: 'Task Two', completed: true },
-        { id: 3, title: 'Task Three', completed: false }
-    ]
+    const tasks = JSON.parse(fs.readFileSync('tasks.json', 'utf-8'));
     res.render('tasks', { tasks });
+});
+
+app.post('/tasks', (req, res) => {
+    const { title } = req.body;
+    const tasks = JSON.parse(fs.readFileSync('tasks.json', 'utf-8'));
+    const newTask = { id: findNextId(tasks), title, completed: false };
+    tasks.push(newTask);
+    fs.writeFileSync('tasks.json', JSON.stringify(tasks, null, 2));
+    res.redirect('/');
 });
 
 app.post('/tasks/:id', (req, res) => {
     const taskId = parseInt(req.params.id);
+    const tasks = JSON.parse(fs.readFileSync('tasks.json', 'utf-8'));
+    const taskIndex = tasks.findIndex(task => task.id === taskId);
+    if (taskIndex === -1) {
+        return res.status(404).send('Task not found');
+    }
+    const { completed } = req.body;
+    console.log(completed);
+    tasks[taskIndex].completed = completed === 'on';
+    fs.writeFileSync('tasks.json', JSON.stringify(tasks, null, 2));
+    res.redirect('/');
 });
 
 
