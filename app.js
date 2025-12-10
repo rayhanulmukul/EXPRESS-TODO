6c756e07ac4b2f34ad07ff670480d7ff8a0ed108
Md Rayhanul Islam <73143090+rayhanulmukul@users.noreply.github.com>
2025-12-10 12:36:50 +0600

    view tasks

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    const tasks = [
+        { id: 1, title: 'Task One', completed: false },
+        { id: 2, title: 'Task Two', completed: true },
+        { id: 3, title: 'Task Three', completed: false }
+    ]
+    res.render('tasks', { tasks });
 });
+
+app.post('/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id);
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
